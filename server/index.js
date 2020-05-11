1cac67a0a541e27741091c9be0c47650b87d6bcd
Nick-Rj <nik.rj10348@gmail.com>
2020-05-11 23:53:04 +0530

    Correction #1

--- server/index.js
+++ server/index.js
@@ -1,62 +1,91 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 const path = require("path");
-const cors = require('cors')
+const cors = require("cors");
 
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 
+// Importing keys file
 const config = require("./config/key");
 
-// const mongoose = require("mongoose");
+// const { Car } = require("./models/cars");
+
+// Atlas: Cloud Platform
 // mongoose
-//   .connect(config.mongoURI, { useNewUrlParser: true })
-//   .then(() => console.log("DB connected"))
-//   .catch(err => console.error(err));
+//   .connect(
+//     "mongodb+srv://nikrj10348:1234qwertyui@userdata-xnrix.mongodb.net/test?retryWrites=true&w=majority",
+//     { useCreateIndex: true, useNewUrlParser: true, useUnifiedTopology: true }
+//   )
+//   .then(() => console.log("DB CONNECTED"))
+//   .catch((err) => console.error(err));
 
-const mongoose = require("mongoose");
-const connect = mongoose.connect(config.mongoURI,
-  {
-    useNewUrlParser: true, useUnifiedTopology: true,
-    useCreateIndex: true, useFindAndModify: false
+mongoose
+  //Adding URL from dev via key.
+  .connect(config.mongoURI, {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
   })
-  .then(() => console.log('MongoDB Connected...'))
-  .catch(err => console.log(err));
+  .then(() => console.log("DB CONNECTED"))
+  .catch((err) => console.error(err));
 
-app.use(cors())
+// Used for provide access to third-party services
+app.use(cors());
 
-//to not get any deprecation warning or error
-//support parsing of application/x-www-form-urlencoded post data
+// Using Middlewares.
 app.use(bodyParser.urlencoded({ extended: true }));
-//to get json data
-// support parsing of application/json type post data
 app.use(bodyParser.json());
 app.use(cookieParser());
 
-app.use('/api/users', require('./routes/users'));
+const routes = require("./routes/users");
+app.use("/api/users", routes);
 
-app.get('/',(req,res)=>{
-  res.send("Server Running on localhost")
-})
-//use this to show the image you have in node js server to client (react js)
-//https://stackoverflow.com/questions/48914987/send-image-path-from-node-js-express-server-to-react-client
+app.get("/", (req, res) => {
+  res.send("Server is running");
+});
+
+// app.post("/api/cars/database", (req, res) => {
+//   const car = new Car(req.body);
+//   car.save((err, carData) => {
+//     if (err)
+//       return res.status(400).json({
+//         message: "Not able to save in Database",
+//         success: false,
+//         err,
+//       });
+//       //Will be returned in postman
+//       return res.status(200).json({
+//       company: car.company,
+//       name: car.name,
+//       modelNum: car.modelNum,
+//       chassis: car.chassis,
+//       color: car.color,
+//       price: car.price,
+//     });
+//   });
+// });
+
 app.use('/uploads', express.static('uploads'));
 
 // Serve static assets if in production
 if (process.env.NODE_ENV === "production") {
 
-  // Set static folder   
-  // All the javascript and css files will be read and served from this folder
-  app.use(express.static("client/build"));
+  // // Set static folder    
+  // // All the javascript and css files will be read and served from this folder
+  // app.use(express.static("client/build"));
 
-  // index.html for all page routes    html or routing and naviagtion
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
-  });
+  // // index.html for all page routes    html or routing and naviagtion
+  // app.get("*", (req, res) => {
+  //   res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
+  // });
+  app.get("/", (req, res) => {
+  res.send("Server is running");
+});
 }
 
-const port = process.env.PORT || 5000
 
-app.listen(port, () => {
-  console.log(`Server Listening on ${port}`)
-});
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`SERVER is up and running at ${PORT}`));
